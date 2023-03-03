24c3f0f18d766605ae11e95b6569d0596c623a1c
Varun Sharma <vsofficialmail@gmail.com>
2023-03-03 23:07:39 +0530

    Initial Commit

---
+++ airplane ground control/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  airplane ground control
+//
+//  Created by Varun Sharma on 03/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ airplane ground control/airplane_ground_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  airplane_ground_controlApp.swift
+//  airplane ground control
+//
+//  Created by Varun Sharma on 03/03/23.
+//
+
+import SwiftUI
+
+@main
+struct airplane_ground_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
