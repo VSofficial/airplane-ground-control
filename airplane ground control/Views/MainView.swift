dbf312e3aebcafe4ea054305901e5a0b9e2aa400
Varun Sharma <vsofficialmail@gmail.com>
2023-03-03 23:15:49 +0530

    First commit

---
+++ airplane ground control/Views/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  airplane ground control
+//
+//  Created by Varun Sharma on 03/03/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
