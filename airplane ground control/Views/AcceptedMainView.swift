704caad7ce1704f2f433e3132f1a91fe50ed492a
adityagi02 <adityatyagi20bcs126@iiitkottayam.ac.in>
2023-03-12 18:51:46 +0530

    Updated UI of mainAcceptedPage with nested H/VStacks.

--- airplane ground control/Views/AcceptedMainView.swift
+++ airplane ground control/Views/AcceptedMainView.swift
@@ -12,113 +12,161 @@ struct AcceptedMainView: View {
         NavigationView {
             
                 
-            VStack(spacing:100) {
-                
-                
-                List() {
-                    
-                    //1
-                    NavigationLink(destination: EmergencyNotifications(), label: {
-                        Button(action: {
-                            buttonAction()
-                        }, label: {
+            VStack(spacing: -1) {
+                    HStack(spacing: -100){
+                        //1
+                        NavigationLink(destination: EmergencyNotifications(), label: {
+                            Button(action: {
+                                buttonAction()
+                            }, label: {
+                            
+                                HStack(spacing: 18){
+                                    Text("Emergency\nNotifications").font(.system(size: 20)).padding([.trailing])
+                                    Text("🔔").font(.system(size: 45)).shadow(color: Color.green ,radius: 12)
+                                }
+                            }).tint(.blue)
+                                .frame(width: 350, height: 78)
+                                .controlSize(.large)
+                                .buttonStyle(.borderedProminent)
+                                .padding()
+                                .shadow(radius: 12)
+                        })
                         
-                                Text("Emergency Notifications")
-                                     .badge(5)
-                            }
-                         
-                        )
-                    })
-                    
-                    
-                    
-                    //2
-                    NavigationLink(destination: EmergencyNotifications(), label: {
-                        Button(action: {
-                            buttonAction()
-                        }, label: {
-                            Text("Chats ")
-                                .badge("Unread 4")
+                        
+                        //2
+                        NavigationLink(destination: EmergencyNotifications(), label: {
+                            Button(action: {
+                                buttonAction()
+                            }, label: {
+                                VStack{
+                                    Text("💬").font(.system(size: 48))
+                                }
+                            }).tint(.green)
+                                .frame(width: 150, height: 70)
+                                .controlSize(.large)
+                                .buttonStyle(.borderedProminent)
+                                .padding()
+                                .shadow(radius: 12)
                         })
-                    })
-                    
-                    
-                  
-                    
-                    //5
-                    NavigationLink(destination: EmergencyNotifications(), label: {
-                        Button(action: {
-                            buttonAction()
-                        }, label: {
-                            Text("My Schuedule")
+                    }.padding([.trailing])
+                
+                    HStack(spacing: -69){
+                        //6
+                        NavigationLink(destination: EmergencyNotifications(), label: {
+                            Button(action: {
+                                buttonAction()
+                            }, label: {
+                                VStack(spacing: 8){
+                                    Text("Call     ").fontWeight(.medium).font(.system(size: 19))
+                                    Image(systemName: "sos").resizable().frame(width: 60.0, height: 25.0)
+                                }
+                            }).tint(.red)
+                                .frame(width: 200, height: 60)
+                                .controlSize(.large)
+                                .buttonStyle(.borderedProminent)
+                                .padding()
+                                .shadow(radius: 12)
                         })
-                    })
-                    
-                    //6
-                    NavigationLink(destination: EmergencyNotifications(), label: {
-                        Button(action: {
-                            buttonAction()
-                        }, label: {
-                            Text("Make a SOS")
-                                .badge("0 brodcasts")
+                        
+                        //5
+                        NavigationLink(destination: EmergencyNotifications(), label: {
+                            Button(action: {
+                                buttonAction()
+                            }, label: {
+                                HStack(spacing: 8){
+                                    Text("My \n Schuedule ").fontWeight(.medium).font(.system(size: 22))
+                                    Text("⏳").font(.system(size: 45))
+                                }
+                            }).tint(.orange)
+                                .frame(width: 250, height: 90)
+                                .controlSize(.large)
+                                .buttonStyle(.borderedProminent)
+                                .padding()
+                                .shadow(radius: 12)
                         })
-                    })
-                    
+                    }.padding([.trailing])
+
                     //7
                     NavigationLink(destination: EmergencyNotifications(), label: {
                         Button(action: {
                             buttonAction()
                         }, label: {
-                            Text("Weather Report")
+                            Text("Weather Report  ").font(.system(size: 33))
                                 .badge("HOT/23F")
-                        })
+                            Text("🌦️").font(.system(size: 52))
+
+                        }).tint(.yellow)
+                            .frame(width: 1000, height: 80)
+                            .controlSize(.large)
+                            .buttonStyle(.borderedProminent)
+                            .padding()
+                            .shadow(radius: 12)
                     })
-                 
-                    
-                    
-                    
                     //3
                     NavigationLink(destination: EmergencyNotifications(), label: {
                         Button(action: {
                             buttonAction()
                         }, label: {
-                            Text("Flight Schedule")
-                        })
+                            Text("Flights Schedule").font(.system(size: 34))
+                                .badge("HOT/23F")
+                            Text("✈️").font(.system(size: 47)).shadow(color: Color.white ,radius: 12)
+                        }).tint(.green)
+                            .frame(width: 1000, height: 90)
+                            .controlSize(.large)
+                            .buttonStyle(.borderedProminent)
+                            .padding()
+                            .shadow(radius: 12)
                     })
-                    
-                    //4
-                    NavigationLink(destination: EmergencyNotifications(), label: {
+                
+                    HStack(spacing: -55){
+                        //4
+                        NavigationLink(destination: EmergencyNotifications(), label: {
+                            
+                            Button(action: {
+                                buttonAction()
+                            }, label: {
+                                VStack{
+                                    VStack(spacing: 4.5){
+                                        Text("Baggage").font(.system(size: 29)).foregroundColor(.brown)
+                                        Text("🧳").font(.system(size: 37)).shadow(radius: 3)
+                                    }
+                                }
+                            }).tint(.white)
+                                .frame(width: 200, height: 110)
+                                .controlSize(.large)
+                                .buttonStyle(.borderedProminent)
+                                .padding()
+                                .shadow(radius: 8)
+                        })
                         
-                        Button(action: {
-                            buttonAction()
-                        }, label: {
-                            Text("Other seats")
+                        NavigationLink(destination: EmergencyNotifications(), label: {
+                            
+                            Button(action: {
+                                buttonAction()
+                            }, label: {
+                                VStack{
+                                    VStack(spacing: 4){
+                                        Text("Runway").font(.system(size: 27))
+                                        Text("🛬  🛫").font(.system(size: 40)).shadow(color: .white, radius: 3)
+                                    }
+                                }
+                            }).tint(Color.gray)
+                                .frame(width: 215, height: 110)
+                                .controlSize(.large)
+                                .buttonStyle(.borderedProminent)
+                                .padding()
+                                .shadow(radius: 8)
                         })
-                    })
-                    
-                   
-                    
-                    
-                    
+                    }.padding([.leading])
                 }
-                
-                .navigationTitle("Home")
-                
-               
-                
-              
+            .navigationTitle("SmartPort").font(.system(size: 45))
             }
-            
         }
     }
     
     func buttonAction() {
         print("pressed")
-        
     }
-
-    
-}
 
 struct AcceptedMainView_Previews: PreviewProvider {
     static var previews: some View {
