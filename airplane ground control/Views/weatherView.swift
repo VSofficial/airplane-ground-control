840edf42d985abca7adea9751f260da0589737c9
adityagi02 <adityatyagi20bcs126@iiitkottayam.ac.in>
2023-03-17 15:29:49 +0530

    Updated UI with some in button descriptions.

---
+++ airplane ground control/Views/weatherView.swift
@@ -0,0 +1,20 @@
+//
+//  weatherView.swift
+//  airplane ground control
+//
+//  Created by Aditya Tyagi  on 17/03/23.
+//
+
+import SwiftUI
+
+struct weatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct weatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        weatherView()
+    }
+}
